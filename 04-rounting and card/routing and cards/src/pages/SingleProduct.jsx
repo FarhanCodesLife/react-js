83d4f4950bafce04242210086733672397637e3d
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-14 03:17:34 +0500

    bhbhb

--- 04-rounting and card/routing and cards/src/pages/SingleProduct.jsx
+++ 04-rounting and card/routing and cards/src/pages/SingleProduct.jsx
@@ -1,20 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
-    const [data, setData] = useState(null);
-    // `useParams` ko React Router v6 mein is liye use kiya jata hai kyunki yeh hume URL mein mojood dynamic parameters ko access karne ki sahulat deta hai. Jab kisi route mein hum variable parts daalte hain, jaise product ID ya user ID, toh `useParams` se hum uss specific value ko retrieve kar sakte hain aur uske mutabiq apni component ko update kar sakte hain. Yeh bohat useful hota hai jab hume URL ke basis par data dikhana hota hai.
-    useEffect(() => {
-        fetch(`https://fakestoreapi.com/products/1`)
-            .then(res => res.json())
-            .then((res) => {
-                console.log(res)
-                setData(res)
-            }).catch((err) => {
-                console.log(err);
-
-            })
-    }, [])
+    console.log("single products");
+    const {id} = useParams()
     
 //     useEffect(()=>{
 //         axios(`https://fakestoreapi.com/products/${id}`)
@@ -32,7 +21,6 @@ const SingleProduct = () => {
 
   return (
 <>
-<h1>VGUGVUTV</h1>
 </>
 
 )
