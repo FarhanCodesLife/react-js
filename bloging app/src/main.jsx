1c45480921f9fe8f408143ef72650213c0168d56
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-21 00:59:59 +0500

    more spcification

--- bloging app/src/main.jsx
+++ bloging app/src/main.jsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+import AllBlogs from './pages/AllBlogs.jsx'
+import Profile from './pages/Profile.jsx'
+
+let router = createBrowserRouter([{
+  path:'/',
+  element:<Layout/>,
+  children:[
+    {
+      path:'',
+      element:<AllBlogs/>
+    },
+    {
+      path:'login',
+      element:<Login/>,
+    },
+    {
+      path:'register',
+      element:<Register/>
+    },
+    {
+      path:'allblogs',
+      element:<AllBlogs/>
+    },
+    {
+      path:'profile',
+      element:<Profile/>
+    },
+  ]
+}])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <>
+  <RouterProvider router={router} >
+
     <App />
-  </StrictMode>,
+  </RouterProvider>
+  </>
 )
