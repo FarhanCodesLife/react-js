a3e20965ff1ed0ba82af803a1dc684386ea0e4f1
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-09-26 15:29:16 +0500

    condition randring

---
+++ prectice/src/component/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+
+    <div style={{}}>
+        
+<h1>admission hogaya he apka </h1>        
+        </div>
+  )
+}
+
+export default Card
